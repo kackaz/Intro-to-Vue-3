cd22a424a94e07afcc57a0f60b930142969101d1
Ruangwit Kamnerdsap <kackaz@gmail.com>
2021-11-02 03:46:10 +0700

    solution

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
             variants: [
                 { id: 2234, color: 'green' },
                 { id: 2235, color: 'blue' },
